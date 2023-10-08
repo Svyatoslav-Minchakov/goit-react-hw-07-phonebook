0b738c2f7868043f34bbe5d36015d43b2f6ae428
Святослав Минчаков <sminchakov@gmail.com>
2023-10-08 14:01:04 +0300

    added project

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,20 @@
+import css from './Filter.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { qwery } from 'redux/sliceFilter';
+
+export const Filter = () => {
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <input
+        className={css.input}
+        value={filter}
+        onChange={evt => dispatch(qwery(evt.currentTarget.value))}
+        type="text"
+        name="filter"
+        placeholder="Find contacts by name"
+      />
+    </>
+  );
+};
